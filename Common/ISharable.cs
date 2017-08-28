3b542ba279ec696ed0350a839a1c34a9acf47012
Johannes Donath <johannesd@torchmind.com>
2017-08-28 03:35:25 +0200

    Added basic definitions for sharable types.

---
+++ Common/ISharable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LiveSplit.dotStart.Common {
+  /// <summary>
+  /// Provides a base to instances which may be shared between multiple components.
+  /// 
+  /// This interface is meant to be used with implementations that internally use reference counts
+  /// in order to decide whether to free resources that keep the instance alive or not (such as
+  /// threads).
+  /// </summary>
+  public interface ISharable : IDisposable {
+
+    /// <summary>
+    /// Claims ownership of an instance.
+    /// 
+    /// When called the internal reference count is incremented. In order to free the respective
+    /// resource, the instance which claimed ownership must call <see cref="IDisposable.Dispose"/>.
+    /// </summary>
+    void Claim();
+  }
+}
---
+++ Common/Splitter/SharableGameMemory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LiveSplit.dotStart.Common.Splitter {
+  /// <summary>
+  /// Provides a sharable game memory implementation which relies on a reference counter in order
+  /// to shut down the backing thread when necessary.
+  /// </summary>
+  public abstract class SharableGameMemory : GameMemory, ISharable {
+
+    /// <summary>
+    /// Stores the total amount of references to this memory instance.
+    /// </summary>
+    private uint _referenceCount;
+
+    /// <inheritdoc />
+    public void Claim() {
+      ++this._referenceCount;
+    }
+    
+    /// <inheritdoc />
+    public void Dispose() {
+      if (this._referenceCount == 0) {
+        throw new InvalidOperationException("Illegal dispose call: Exceeded amount of known owners");
+      }
+      
+      if (--this._referenceCount == 0) {
+        Debug.WriteLine("[Sharable Game Memory] Last reference disposed - Destroying remaining threads");
+
+        if (this.Alive) {
+          this.Stop();
+        }
+      }
+    }
+  }
+}
